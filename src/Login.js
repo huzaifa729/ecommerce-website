82d447a7a02046e40e2ed1a294020f87da94c840
Huzaifa dabir <73576043+huzaifa729@users.noreply.github.com>
2022-07-19 04:44:22 -0700

    updated

--- src/Login.js
+++ src/Login.js
@@ -89,16 +89,18 @@ function Login() {
 export default Login;
 
 const Container = styled.div`
-     background-image: url("https://abs.twimg.com/sticky/illustrations/lohp_en_1302x955.png");
+      /* background-image: url("https://png.pngtree.com/thumb_back/fh260/background/20210224/pngtree-blue-abstract-background-business-image_564246.jpg");  */
+    /* background-image: url("https://wallpaper.dog/large/10824481.jpg"); */
+    background-image: url("https://t3.ftcdn.net/jpg/01/91/78/32/360_F_191783282_0TVrx5VrvrkpDHSKdjjI87HkbXJy5TMw.jpg");
      color:black;
-     height: 100vh;
+     height: 656px;
      background-position: center;
      background-size: cover;
-    
+  
 `;
 
 const HeaderLogo = styled.div`
-    color: black;
+   color:white;
     display:flex;
     flex-direction:column;
   margin-left:55px;
@@ -106,22 +108,24 @@ const HeaderLogo = styled.div`
 
     img{
        margin-left:30px;
-     max-width:45px;
+     max-width:55px;
       object-fit:contain;
      margin-top:-4px;
     }
 `
 
  const Title = styled.span`
-     font-size:35px;
-   font-family: Verdana, Arial, Helvetica, sans-serif;  
+     font-weight: 800;
+     font-size:77px;
+  font-family: 'Qwitcher Grypen', cursive; 
      background: rgb(123,136,136);
 background: linear-gradient(0deg, rgba(123,136,136,1) 0%, rgba(0,0,0,1) 100%);
             -webkit-text-fill-color: transparent;
-            /* -webkit-background-clip: text; */
               -webkit-background-clip: text; 
+              -webkit-background-clip: text;  
               text-shadow: 2px 2px;
-               color: beige; 
+              color:whitesmoke;
+               margin-top: 10px;
            /* color:white; */
         
 `
@@ -129,23 +133,25 @@ background: linear-gradient(0deg, rgba(123,136,136,1) 0%, rgba(0,0,0,1) 100%);
 const LoginCenter = styled.div`
         text-align:center;
         align-items:center;
-        margin-top:5px;
+        margin-top:-90px;
 `
 
 const LoginTitle = styled.div`
-     color:white;
-     font-size:45px;
-     font-family:Arial, Helvetica, sans-serif;
+     color:whitesmoke;
+     font-size:65px;
+     font-family: 'Qwitcher Grypen', cursive;
      letter-spacing:7px;
      text-decoration:underline;
+      font-weight: 700;
 `
 
 const LoginPara = styled.div`
-  color:white;
-  font-size:27px;
-  font-family:Arial, Helvetica, sans-serif;
-  margin-top:12px;
+  color:whitesmoke;
+  font-size:77px;
+  font-family: 'Qwitcher Grypen', cursive;
+  margin-top:-18px;
   letter-spacing:0.7px;
+  font-weight: 700;
 `
 
 const LoginPage = styled.div`
@@ -154,23 +160,27 @@ const LoginPage = styled.div`
     // border: 1px solid #2a405b;
     // background-color: #1c2a3b;
    padding: 00px 6px;
-   color:black;
+   color:whitesmoke;
     width: 25%;
     height: 340px;
     font-size: 16px;
     border-radius: 3px;
     margin:auto;
-    margin-top:50px;
+    margin-top:-35px;
 `
 
 
 const LName = styled.div`
-  color:white;
-  font-size:24px;
-   font-family: Verdana, Geneva, Tahoma, sans-serif;
-  margin-top:10px;
+ color:whitesmoke;
+   /* font-size: 40px; */
+  margin-top:33px;
   margin-left:10px;
   text-decoration:underline;
+  font-weight: 700;
+  font-size:35px;
+  font-style: italic;
+   /* text-align: center;
+  justify-content: center;  */
 `
 
 
@@ -178,10 +188,10 @@ const SignWith = styled.div`
   margin-top: 20px; 
   margin-left: 10px;
   text-align: center;
-  font-size: 18px;
+  font-size: 20px;
   /* color: #5b728c; */
-  color:white;
-  font-weight: 540;
+  color:whitesmoke;
+  font-weight: 500;
   font-family: Verdana, Geneva, Tahoma, sans-serif;
 
    ::before{
@@ -218,25 +228,32 @@ const LoginBtns = styled.div`
 
 `
 const Google = styled.button`
- background-color: #2a405b;
+ /* background-color: #2a405b; */
  width:150px;
  height:41px;
  border-radius:5px;
  display:flex;
  align-items:center;
-
+ background: none;
+ /* background-color: #2874f0; */
+  /* background: rgb(123,136,136);
+background: linear-gradient(0deg, rgba(123,136,136,1) 0%, rgba(0,0,0,1) 100%);  */
  cursor: pointer;
    img{
     height:28px;
     margin-left:6px;
     margin-top:0px;
+
    }
 
   
 `
 
 const Facebook = styled.button`
- background-color: #2a405b;
+ /* background-color: #2a405b; */
+ background: none;
+ /* background: rgb(123,136,136);
+background: linear-gradient(0deg, rgba(123,136,136,1) 0%, rgba(0,0,0,1) 100%); */
  width:151px;
  height:41px;
  border-radius:5px;
@@ -251,7 +268,10 @@ cursor: pointer;
 `
 
 const Twitter = styled.button`
- background-color: #2a405b;
+ /* background-color: #2a405b; */
+ /* background: rgb(123,136,136);
+background: linear-gradient(0deg, rgba(123,136,136,1) 0%, rgba(0,0,0,1) 100%); */
+  background: none; 
  width:150px;
  height:41px;
  border-radius:5px;
@@ -267,7 +287,10 @@ cursor: pointer;
 `
 
 const Github = styled.button`
- background-color: #2a405b;
+ /* background-color: #2a405b; */
+ /* background: rgb(123,136,136);
+background: linear-gradient(0deg, rgba(123,136,136,1) 0%, rgba(0,0,0,1) 100%); */
+  background: none; 
  width:150px;
  height:41px;
  border-radius:5px;
@@ -284,12 +307,12 @@ cursor: pointer;
 
 
 const GP = styled.div`
- font-size:20px;
+ font-size:21px;
  font-family: Verdana, Geneva, Tahoma, sans-serif;
  margin-left:7px;
- color:whitesmoke;
+ color:whitesmoke; 
  margin-top:2px;
-
+font-weight: 525;
 
 `
 
@@ -313,18 +336,20 @@ const TP = styled.div`
 const Line = styled.div`
   width:100%;
   height: 2px;
-  background-color: #2a405b;
-  margin-top:22px;
+  background-color: black;
+  margin-top:55px;
 `
 
 const Foot = styled.div`
-   color:white;
-   font-family: Verdana, Geneva, Tahoma, sans-serif;
-   font-size:19px;
+  color:whitesmoke;
+   /* font-family: Verdana, Geneva, Tahoma, sans-serif; */
+   font-size:65px;
+  font-family: 'Qwitcher Grypen', cursive;
    text-align:center;
    justify-content:center;
    margin-top:12px;
   margin-left:5px;
+  font-weight: bolder;
 `
 
 
@@ -332,8 +357,10 @@ const Create = styled.div`
    margin-top:12px;
 `
 const Account = styled.div`
-  font-size:18px;
-  font-family:Verdana, Geneva, Tahoma, sans-serif;
+  font-size:25px;
+  /* font-family: 'Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode', Geneva, Verdana, sans-serif; */
+  font-style:italic;
+ font-weight: 630;
 `
 
 
