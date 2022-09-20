88d6fe33da9ff25b7b0ccbf4dbe86a9e347501e7
Huzaifa dabir <73576043+huzaifa729@users.noreply.github.com>
2022-09-20 05:30:23 -0700

    updated

--- src/Rigester.js
+++ src/Rigester.js
@@ -3,12 +3,75 @@ import styled from 'styled-components';
 
 function Rigester() {
   return (
-    <Container>Rigester bnxgb b</Container>
+    <Container>
+    <LoginForm>
+       <First>
+       <Label>Name</Label>
+       <InputBox type="text"></InputBox>
+       </First>
+       <First>
+       <Label>Email</Label>
+       <InputBox type="text"></InputBox>
+       </First>
+       <First>
+       <Label>Password</Label>
+       <InputBox type="password" name="pass"></InputBox>
+       </First>
+       <Button>Continue</Button>
+    </LoginForm>
+  </Container>
   )
 }
 
 export default Rigester;
 
 const Container = styled.div`
-   color:red;
+ margin-right:210px;
+ color: white;
+ margin-top: -7px;
+`
+const LoginForm = styled.div`
+  display: flex;
+  flex-direction: column;
+`
+
+const First = styled.div`
+display: block;
+flex-direction: column;
+margin-bottom: 30px;
+`
+
+const Label = styled.div`
+ color: white;
+ font-family: Arial, Helvetica, sans-serif;
+ font-size: 21px;
+ letter-spacing: 0.1px;
+ margin-bottom: 3px;
+`
+
+const InputBox = styled.input`
+  border: none;
+  outline: none;
+  background: none;
+  width: 250px;
+  border-bottom: 3px solid grey;
+  /* margin-left: 190px; */
+  color: white;
+  font-family: Arial, Helvetica, sans-serif;
+  font-size: 19px;
+`
+
+const Button = styled.button`
+    font-size: 22px;
+    text-align: center;
+    justify-content: center;
+    border: none;
+    outline: none;
+    background: none;
+    border: 5px solid black;
+    font-family: Arial, Helvetica, sans-serif;
+    width: 150px;
+    color: white;
+    cursor: pointer;
+    margin-top: 5px;
 `
--- src/Sign.js
+++ src/Sign.js
@@ -3,7 +3,22 @@ import styled from 'styled-components';
 
 function Sign() {
   return (
-    <Container>Sign gvgsvgvgsvgs </Container>
+    <Container>
+     <LoginForm>
+        <First>
+        <Label>Name</Label>
+        <InputBox type="text"></InputBox>
+        </First>
+        <First>
+        <Label>Email</Label>
+        <InputBox type="text"></InputBox>
+        </First>
+        <First>
+        <Label>Password</Label>
+        <InputBox type="password" name="pass"></InputBox>
+        </First>
+     </LoginForm>
+   </Container>
   )
 }
 
@@ -11,6 +26,37 @@ export default Sign;
 
 
 const Container = styled.div`
-   color:red;
+ margin-right:210px;
+ color: white;
+ margin-top: -7px;
+`
+const LoginForm = styled.div`
+  display: flex;
+  flex-direction: column;
+`
 
+const First = styled.div`
+display: block;
+flex-direction: column;
+margin-bottom: 30px;
+`
+
+const Label = styled.div`
+ color: white;
+ font-family: Arial, Helvetica, sans-serif;
+ font-size: 21px;
+ letter-spacing: 0.1px;
+ margin-bottom: 3px;
+`
+
+const InputBox = styled.input`
+  border: none;
+  outline: none;
+  background: none;
+  width: 250px;
+  border-bottom: 3px solid grey;
+  /* margin-left: 190px; */
+  color: white;
+  font-family: Arial, Helvetica, sans-serif;
+  font-size: 19px;
 `
