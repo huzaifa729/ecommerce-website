c386332afb014dd20f07c14c0b212028e15b4865
Huzaifa dabir <73576043+huzaifa729@users.noreply.github.com>
2022-09-20 10:32:20 -0700

    init

--- src/Sign.js
+++ src/Sign.js
@@ -3,12 +3,9 @@ import styled from 'styled-components';
 
 function Sign() {
   return (
+    <Signn>
     <Container>
      <LoginForm>
-        <First>
-        <Label>Name</Label>
-        <InputBox type="text"></InputBox>
-        </First>
         <First>
         <Label>Email</Label>
         <InputBox type="text"></InputBox>
@@ -19,20 +16,46 @@ function Sign() {
         </First>
      </LoginForm>
    </Container>
+   <SignWith>Sign up with</SignWith>
+   <SocialAccount>
+       <Google>
+        <Button>
+        <img className="goggle-logo" src="//assets-netstorage.groww.in/web-assets/billion_groww_desktop/prod/build/client/images/google-icon.5c764c55.svg" alt="Google Logo"/>
+    
+        </Button>
+       </Google>
+
+       <Google>
+        <Button>
+        <img className="twitter-logo" src="https://cdn-icons-png.flaticon.com/128/747/747622.png?ga=GA1.2.936411358.1657092341" alt="twitter"/>
+      
+        </Button>
+       </Google>
+
+       <Google>
+        <Button>
+        <img className="github-logo" src="https://cdn-icons-png.flaticon.com/512/25/25231.png" alt="Github Logo"/>
+        </Button>
+       </Google>
+   </SocialAccount>
+   </Signn>
   )
 }
 
 export default Sign;
 
+const Signn = styled.div`
+
+`
 
 const Container = styled.div`
- margin-right:210px;
+ margin-right:200px;
  color: white;
  margin-top: -7px;
 `
 const LoginForm = styled.div`
-  display: flex;
-  flex-direction: column;
+   display: flex;
+  flex-direction: column; 
 `
 
 const First = styled.div`
@@ -60,3 +83,65 @@ const InputBox = styled.input`
   font-family: Arial, Helvetica, sans-serif;
   font-size: 19px;
 `
+
+const SignWith = styled.div`
+   color:white;
+   text-align: center;
+   font-size: 20px;
+   font-family: Arial, Helvetica, sans-serif;
+   justify-content:center;
+
+   ::before{
+    content: "";
+display: inline-block;
+    height: 3px;
+    width: 27%;
+    background-color: #2a405b;
+    position: relative;
+    vertical-align: middle; 
+     left: -20px; 
+   }
+
+   ::after{
+    content: "";
+    width: 27%;
+    height: 3px;
+    background-color: #2a405b;
+    position: relative;
+    display: inline-block;
+    vertical-align: middle;
+    left: 25px;
+   }
+`
+
+const SocialAccount = styled.div`
+ display: flex;
+ /* flex-direction: column; */
+ margin-top: 20px;
+ text-align: center;
+ /* justify-content: center; */
+ margin-right: 45px;
+ justify-content: space-around;
+`
+
+const Google = styled.div`
+ margin-bottom: 15px;
+ margin-left: 25px;
+`
+
+const Button = styled.button`
+
+width: 127%;
+    height: 48px;
+    background-color: #2a405b;
+    border-radius: 5px;
+   margin: 0px 12px;
+   
+  cursor: pointer;
+  img{
+    width: 30px;
+  }
+`
+
+
+
